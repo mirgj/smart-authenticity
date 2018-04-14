fcf25581d538222bc1aaef9d15366991a488d669
mirgj <mirash.gjolaj@gmail.com>
2018-04-14 16:57:55 +0800

    improved test

--- test/Authenticity.js
+++ test/Authenticity.js
@@ -1,6 +1,7 @@
 const Authenticity = artifacts.require('./Authenticity.sol');
+const Company = artifacts.require('./Company.sol');
 
-contract("Authenticity", function (accounts) {
+contract('Authenticity', function (accounts) {
   const cpId = 'MSF';
   const prId = web3.sha3('ABC-123');
   const requester = accounts[0];
@@ -19,7 +20,7 @@ contract("Authenticity", function (accounts) {
     }
   });
 
-  it("should fail to get the authenticity for a non existing company", async () => {
+  it('should fail to get the authenticity for a non existing company', async () => {
     let isErr = false;
 
     try {
@@ -27,17 +28,17 @@ contract("Authenticity", function (accounts) {
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("the company should be unregistered", async () => {
+  it('the company should be unregistered', async () => {
     const result = await authenticityContract.isCompanyRegistered(cpId);
 
-    assert.isFalse(result, "It should be unregistered");
+    assert.isFalse(result, 'It should be unregistered');
   });
 
-  it("should fail to create a company with empty identifier", async () => {
+  it('should fail to create a company with empty identifier', async () => {
     let isErr = false;
 
     try {
@@ -45,11 +46,11 @@ contract("Authenticity", function (accounts) {
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("should fail to create a company with empty fullname", async () => {
+  it('should fail to create a company with empty fullname', async () => {
     let isErr = false;
 
     try {
@@ -57,11 +58,11 @@ contract("Authenticity", function (accounts) {
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("should fail to create a company with empty location", async () => {
+  it('should fail to create a company with empty location', async () => {
     let isErr = false;
 
     try {
@@ -69,25 +70,25 @@ contract("Authenticity", function (accounts) {
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("should create a new company", async () => {
+  it('should create a new company', async () => {
     const tx = await authenticityContract.createCompany(cpId, 'My company', 'My location', {from: company1});
-  
+
     assert.equal(tx.receipt.status, 1, 'Wrong status');
-    assert.equal(tx.logs.length, 1, "One event expected");
-    assert.equal(tx.logs[0].event, "CompanyCreated", "CompanyCreated event should be fired");
-    assert.equal(tx.logs[0].args.identifier, cpId, "Wrong identifier");
-    assert.equal(tx.logs[0].args.fullName, 'My company', "Wrong fullName");
-    assert.equal(tx.logs[0].args.location, 'My location', "Wrong location");
-    assert.isTrue(web3.isAddress(tx.logs[0].args.addr), "Should be an address");
+    assert.equal(tx.logs.length, 1, 'One event expected');
+    assert.equal(tx.logs[0].event, 'CompanyCreated', 'CompanyCreated event should be fired');
+    assert.equal(tx.logs[0].args.identifier, cpId, 'Wrong identifier');
+    assert.equal(tx.logs[0].args.fullName, 'My company', 'Wrong fullName');
+    assert.equal(tx.logs[0].args.location, 'My location', 'Wrong location');
+    assert.isTrue(web3.isAddress(tx.logs[0].args.addr), 'Should be an address');
 
     company1Address = tx.logs[0].args.addr;
   });
 
-  it("should fail to create again the same company", async () => {
+  it('should fail to create again the same company', async () => {
     let isErr = false;
 
     try {
@@ -95,29 +96,29 @@ contract("Authenticity", function (accounts) {
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("the company should be registered", async () => {
+  it('the company should be registered', async () => {
     const result = await authenticityContract.isCompanyRegistered(cpId);
 
-    assert.isTrue(result, "It should be registered correctly");
+    assert.isTrue(result, 'It should be registered correctly');
   });
 
-  it("should fail to add a product for a company that doesn't exist", async () => {
+  it('should fail to add a product for a company that doesn\'t exist', async () => {
     let isErr = false;
 
     try {
-      const tx = await authenticityContract.addProduct('', 'my_serial_hash', 'my_product_name', {from: company1});
+      const tx = await authenticityContract.addProduct('', prId, 'my_product_name', {from: company1});
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("should fail to add a product with an empty serial number", async () => {
+  it('should fail to add a product with an empty serial number', async () => {
     let isErr = false;
 
     try {
@@ -125,46 +126,57 @@ contract("Authenticity", function (accounts) {
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("should fail to add a product with an empty product name", async () => {
+  it('should fail to add a product with an empty product name', async () => {
     let isErr = false;
 
     try {
-      const tx = await authenticityContract.addProduct(cpId, 'my_serial_hash', '', {from: company1});
+      const tx = await authenticityContract.addProduct(cpId, prId, '', {from: company1});
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("should fail to add the product if the call is not performed by the company owner", async () => {
+  it('should fail to add the product if the call is not performed by the company owner', async () => {
     let isErr = false;
 
     try {
-      const tx = await authenticityContract.addProduct(cpId, 'my_serial_hash', 'my_product_name', {from: company2});
+      const tx = await authenticityContract.addProduct(cpId, prId, 'my_product_name', {from: company2});
     } catch(err) {
       isErr = true;
     } finally {
-      assert.isTrue(isErr, "It should throw an error");
+      assert.isTrue(isErr, 'It should throw an error');
     }
   });
 
-  it("should add the product correctly", async () => {
-  	const tx = await authenticityContract.addProduct(cpId, 'my_serial_hash', 'my_product_name', {from: company1});
-  	console.log(tx.receipt.logs);
+  it('should add the product correctly', async () => {
+    const company1Instance = await Company.at(company1Address);
+    let hasFired = false;
+
+    const events = company1Instance.allEvents();
+    events.watch((err, res) => {
+      if (err) throw err;
+        
+      hasFired = true;
+      assert.equal(res.event, 'ProductAdded', 'ProductAdded event should be fired');
+      assert.equal(res.args.companyAddress, company1Address, 'Wrong identifier');
+      assert.equal(res.args.serialNumber, prId, 'Wrong serial hash');
+      assert.equal(res.args.name, 'my_product_name', 'Wrong product name');
+      assert.isTrue(web3.isAddress(res.args.productAddress), 'Should be an address');
+
+      product1Address = res.args.addr;
+    });
+
+  	const tx = await authenticityContract.addProduct(cpId, prId, 'my_product_name', {from: company1});
+    
     assert.equal(tx.receipt.status, 1, 'Wrong status');
-    assert.equal(tx.logs.length, 1, "One event expected");
-    assert.equal(tx.logs[0].event, "ProductAdded", "ProductAdded event should be fired");
-    assert.equal(tx.logs[0].args.companyAddress, company1Address, "Wrong identifier");
-    assert.equal(tx.logs[0].args.serialNumber, 'my_serial_hash', "Wrong serial hash");
-    assert.equal(tx.logs[0].args.name, 'my_product_name', "Wrong product name");
-    assert.isTrue(web3.isAddress(tx.logs[0].args.productAddress), "Should be an address");
-
-    product1Address = tx.logs[0].args.addr;
+    assert.isTrue(hasFired, 'expected event');
+    events.stopWatching();
   });
 
 });
